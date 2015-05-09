f3b1d894cf66d7eec75011489e0ff93d64465425
Seva Zhidkov <zhidkovseva@gmail.com>
2015-05-09 20:00:15 +0400

    Added tweets supporting

--- parse_news.py
+++ parse_news.py
@@ -1,5 +1,7 @@
 import feedparser
 import json
+import requests
+from pyquery import PyQuery
 from send_message import send_vk_message, send_telegram_message
 
 subscribers = json.loads(open('send_to.json').read())
@@ -18,7 +20,29 @@ def get_5_latest_news():
 
 
 def get_3_best_tweets():
-    return ['Последние твиты:']
+    best_tweets = ['Лучшие твиты:']
+    # HTML query in jQuery style to get best tweets
+    html_query = '.container .b-content .b-content-wrapper ' + \
+                 '.b-content__b1 .b-block .b-articles ' + \
+                 '.b-articles__b'
+    tj_tweets_page = requests.get('http://tjournal.ru/tweets').text
+    tweets_html = PyQuery(tj_tweets_page)
+    for i in range(3):
+        html_tweet_query = html_query + ':eq( {} )'.format(str(i))
+        tweet_html = tweets_html(html_tweet_query)
+        tweet_text = tweet_html('.b-articles__b__text').text()
+        tweet_author = tweet_html('.b-articles__b__name').text()
+        if tweet_html('.b-articles__b__picture'):
+            tweet_picture_html = tweet_html('.b-articles__b__picture a')
+            tweet_picture = tweet_picture_html.attr('href')
+        else:
+            tweet_picture = None
+        best_tweets.append('{author}: "{tweet}"<br>{picture}'.format(
+            author=tweet_author,
+            tweet=tweet_text,
+            picture=tweet_picture if tweet_picture is not None else ''
+        ))
+    return best_tweets
 
 digest = get_5_latest_news() + get_3_best_tweets()
 for subscriber in subscribers:
