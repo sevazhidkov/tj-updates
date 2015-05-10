a4faa46624ef7ad2c6d9d6b356e26f8af74b4721
Seva Zhidkov <zhidkovseva@gmail.com>
2015-05-10 08:58:14 +0400

    Changed name of function for getting latest news

--- parse_news.py
+++ parse_news.py
@@ -7,7 +7,7 @@ from send_message import send_vk_message, send_telegram_message
 subscribers = json.loads(open('send_to.json').read())
 
 
-def get_5_latest_news():
+def get_3_latest_news():
     latest_news = ['Последние новости:']
     news = feedparser.parse('http://tjournal.ru/rss')['items']
     # Get 5 news
