40357a8ba54330fd2d9aa35e3c203800c26a1ac5
Seva Zhidkov <zhidkovseva@gmail.com>
2015-05-24 12:54:59 +0400

    Added telegram support

--- send_message.py
+++ send_message.py
@@ -2,9 +2,10 @@ from time import sleep
 from datetime import datetime
 from vk_api import VkApi
 from config import VK_LOGIN, VK_PASSWORD
+from pytg.sender import Sender
 
 vk = VkApi(VK_LOGIN, VK_PASSWORD)
-
+telegram = Sender(host="localhost", port=4458)
 
 def create_welcome_message():
     now_hour = datetime.timetuple(datetime.now())[3]
@@ -16,14 +17,22 @@ def create_welcome_message():
         welcome_message = 'Добрый вечер'
     return welcome_message
 
+welcome_message = create_welcome_message()
+
 
 def send_telegram_message(messages, user_id, user_name):
-    #TODO: Реализовать работу с Telegram
+    telegram.send_msg(user_id, '{welcome_message}, {name}'.format(
+        welcome_message=welcome_message,
+        name=user_name
+    ))
+    telegram.send_msg(user_id, 'Я курьер TJ, меня зовут Сева.')
+    for message in messages:
+        telegram.send_msg(user_id, message)
+        sleep(2)
     return None
 
 
 def send_vk_message(messages, user_id, user_name):
-    welcome_message = create_welcome_message()
     vk.method('messages.send', {
             'message': '{welcome_message}, {name}'.format(
                 welcome_message=welcome_message,
